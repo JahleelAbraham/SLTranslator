9a2f03dad048e30f34d14357f274e2235824045d
Jahleel Abraham <jahleelabraham@hotmail.com>
2023-10-13 14:49:53 +0100

    Inital Commit

--- dataset.py
+++ dataset.py
@@ -3,8 +3,14 @@ import pandas as pd
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 
-class ASLDataset(Dataset):
+class ASLDataset(Dataset): # TODO: Create dataset type
     def __init__(self, dataCsv, transform=None, target_transform=None):
-        self.dataCsv = dataCsv
+        self.dataCsv = pd.read_csv(dataCsv)
         self.transform = transform
         self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.dataCsv.rows)
+
+    def __getitem__(self, i):
+        return self.dataCsv.rows[i+1][self.dataCsv.columns > 0]
---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = pd.read_csv("./training/data.csv")
+
+print()  # TODO: Figure out how to iterate through rows
