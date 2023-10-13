7b164f38c9b22c581fa919aadf8be12796cedc6e
Jahleel Abraham <jahleelabraham@hotmail.com>
2023-10-13 20:16:27 +0100

    Reformatting because PyCharm hates me

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 
 read = pd.read_csv("./training/data.csv").to_numpy()
 
+
 def delete_label(item):
     return np.delete(item, 0)
 
@@ -13,7 +14,6 @@ def extract_label(arr):
 
 data = list(map(delete_label, read))
 labels = list(map(extract_label, read))
-
 
 for i in range(0, len(data)):
     print(f"Item #{i}- Label: {data[i][0]} - Data: {np.delete(data[i], 0)}")
